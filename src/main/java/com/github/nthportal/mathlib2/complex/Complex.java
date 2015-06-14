d1ea741b8bb1b55e875f5cfb81143beadb05cdb0
NthPortal <mail.vanpelt@gmail.com>
2015-06-14 16:52:19 -0400

    Refactored Complex.complex to imaginary

--- src/main/java/com/github/nthportal/mathlib2/complex/Complex.java
+++ src/main/java/com/github/nthportal/mathlib2/complex/Complex.java
@@ -1,27 +1,35 @@
 package com.github.nthportal.mathlib2.complex;
 
-public class Complex {
-    public final int real;
-    public final int complex;
+import java.util.Random;
 
-    public Complex(int real, int complex) {
+public class Complex {
+    private static final Random random = new Random();
+
+    public final int real;
+    public final int imaginary;
+
+    public Complex(int real, int imaginary) {
         this.real = real;
-        this.complex = complex;
+        this.imaginary = imaginary;
     }
 
     public Complex add(Complex c) {
-        return new Complex((real + c.real), (complex + c.complex));
+        return new Complex((real + c.real), (imaginary + c.imaginary));
     }
 
     public Complex subtract(Complex c) {
-        return new Complex((real - c.real), (complex - c.complex));
+        return new Complex((real - c.real), (imaginary - c.imaginary));
     }
 
     public Complex multiply(Complex c) {
-        return new Complex((real * c.real) - (complex * c.complex), (real * c.complex) + (complex * c.real));
+        return new Complex((real * c.real) - (imaginary * c.imaginary), (real * c.imaginary) + (imaginary * c.real));
     }
 
     public Complex conjugate() {
-        return new Complex(real, -complex);
+        return new Complex(real, -imaginary);
+    }
+
+    public static Complex random() {
+        return new Complex(random.nextInt(), random.nextInt());
     }
 }
