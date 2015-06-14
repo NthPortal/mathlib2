5c3bba07fceda70327e32e7a54a333380e9d03c0
NthPortal <mail.vanpelt@gmail.com>
2015-06-14 19:36:37 -0400

    Created empty tests

---
+++ src/test/java/com/github/nthportal/mathlib2/matrix/MatrixTest.java
@@ -0,0 +1,75 @@
+package com.github.nthportal.mathlib2.matrix;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MatrixTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testBean() {
+
+    }
+
+    @Test
+    public void testCreate() throws Exception {
+
+    }
+
+    @Test
+    public void testGet() throws Exception {
+
+    }
+
+    @Test
+    public void testAdd() throws Exception {
+
+    }
+
+    @Test
+    public void testSubtract() throws Exception {
+
+    }
+
+    @Test
+    public void testMultiply() throws Exception {
+
+    }
+
+    @Test
+    public void testMultiply1() throws Exception {
+
+    }
+
+    @Test
+    public void testTranspose() throws Exception {
+
+    }
+
+    @Test
+    public void testIdentity() throws Exception {
+
+    }
+
+    @Test
+    public void testHashCode() throws Exception {
+
+    }
+
+    @Test
+    public void testEquals() throws Exception {
+
+    }
+}
